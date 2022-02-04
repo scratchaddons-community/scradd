56953fde92b7ab0d26c5e8d3ae9bd3ac41d39bf0
RedGuy12 <paul@reid-family.org>
2022-02-03 21:28:56 -0600

    fix regex again

--- common/suggest.js
+++ common/suggest.js
@@ -84,7 +84,7 @@ export default class SuggestionBuilder {
 
 		await Promise.all([
 			interaction.channel.setName(
-				interaction.channel.name.replace(/^(?:.+? \| )?([^|]+)$/i, answer),
+				interaction.channel.name.replace(/^[^|]+?(?=( \| .+)?$)/i, answer),
 				"Thread answered by " + interaction.user.tag,
 			),
 
