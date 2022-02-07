c089dcae7a0bb35eac251f5714d299302e9d435f
RedGuy12 <paul@reid-family.org>
2022-02-06 18:45:58 -0600

    modmail tweaks - use awaits - add permissions
    Signed-off-by: RedGuy12 <paul@reid-family.org>

--- commands/modmail.js
+++ commands/modmail.js
@@ -8,57 +8,75 @@ if (!MODMAIL_CHANNEL) throw new Error("MODMAIL_CHANNEL is not set in the .env.")
 
 /** @type {import("../types/command").default} */
 const info = {
-	data: new SlashCommandBuilder().setDescription(" ").addSubcommand((subcommand) =>
-		subcommand
-			.setName("close")
-			.setDescription("(Mods only) Close a modmail ticket.")
-			.addStringOption((input) =>
-				input
-					.setName("reason")
-					.setDescription("Reason for closing the ticket")
-					.setRequired(true),
-			),
-	),
+	data: new SlashCommandBuilder()
+		.setDefaultPermission(false)
+		.setDescription(" ")
+		.addSubcommand((subcommand) =>
+			subcommand
+				.setName("close")
+				.setDescription("(Mods only) Close a modmail ticket.")
+				.addStringOption((input) =>
+					input
+						.setName("reason")
+						.setDescription("Reason for closing the ticket")
+						.setRequired(true),
+				),
+		),
 
 	async interaction(interaction) {
 		if (
 			interaction.channel?.type !== "GUILD_PUBLIC_THREAD" ||
 			interaction.channel.parentId !== MODMAIL_CHANNEL
-		)
+		) {
+			interaction.reply({
+				content: `This command can only be used in threads in <#${MODMAIL_CHANNEL}>.`,
+				ephemeral: true,
+			});
 			return;
+		}
 		const command = interaction.options.getSubcommand();
 		if (command === "close") {
-			await interaction.reply({
-				content: `:white_check_mark: Modmail ticket closed!`,
-			});
+			const promises = [];
+			promises.push(
+				interaction.reply({
+					content: `:white_check_mark: Modmail ticket closed!`,
+				}),
+			);
 			const starter = await interaction.channel.fetchStarterMessage();
 			const user = await interaction.client.users.fetch(
 				interaction.channel?.name.match(/^.+ \((\d+)\)$/i)?.[1] || "",
 			);
 			if (!user) return;
 			const dm = await user.createDM();
-			dm.send({
-				embeds: [
-					new MessageEmbed()
-						.setTitle("Modmail ticket closed!")
-						.setDescription(interaction.options.getString("reason") || "")
-						.setTimestamp(starter.createdTimestamp),
-				],
-			});
+			promises.push(
+				dm.send({
+					embeds: [
+						new MessageEmbed()
+							.setTitle("Modmail ticket closed!")
+							.setDescription(interaction.options.getString("reason") || "")
+							.setTimestamp(starter.createdTimestamp)
+						.setColor(0x008000),
+					],
+				}),
+			);
 
-			await starter.edit({
-				embeds: [
-					{
-						title: "Modmail ticket closed",
-						description: starter.embeds[0]?.description || "",
-						color: 0x00ff00,
-					},
-				],
-			});
-			interaction.channel.setLocked(true, "Closed by " + interaction.user.tag);
-			interaction.channel.setArchived(true, "Closed by " + interaction.user.tag);
+			promises.push(
+				starter.edit({
+					embeds: [
+						{
+							title: "Modmail ticket closed",
+							description: starter.embeds[0]?.description || "",
+							color: 0x008000,
+						},
+					],
+				}),
+			);
+			await Promise.all(promises);
+			await interaction.channel.setLocked(true, "Closed by " + interaction.user.tag);
+			await interaction.channel.setArchived(true, "Closed by " + interaction.user.tag);
 		}
 	},
+	permissions: [{ id: process.env.MODERATOR_ROLE || "", type: "ROLE", permission: true }],
 };
 
 export default info;
