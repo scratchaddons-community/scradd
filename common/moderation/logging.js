73686e4efcb8e566eeb5aae12987e78bbad00446
RedGuy12 <paul@reid-family.org>
2022-05-05 10:30:30 -0500

    allowedMentions: { users: [] },
    Signed-off-by: RedGuy12 <paul@reid-family.org>

--- common/moderation/logging.js
+++ common/moderation/logging.js
@@ -8,5 +8,5 @@ export default async function log(guild, content, group, extra) {
 	const channel = await guild.channels.fetch(process.env.LOGS_CHANNEL || "");
 	if (!channel?.isText()) throw new TypeError("Channel is not a text channel");
 	channel.threads;
-	await channel.send({ content, ...extra });
+	await channel.send({ ...extra, content, allowedMentions: { users: [] } });
 }
