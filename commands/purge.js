52f758d8eb70b284eb4d402f5be72c751c274ae0
RedGuy12 <paul@reid-family.org>
2022-08-07 16:57:43 -0500

    add /purge
    Signed-off-by: RedGuy12 <paul@reid-family.org>

---
+++ commands/purge.js
@@ -0,0 +1,70 @@
+import { SlashCommandBuilder } from "@discordjs/builders";
+import CONSTANTS from "../common/CONSTANTS.js";
+
+/** @type {import("../types/command").default} */
+const info = {
+	data: new SlashCommandBuilder()
+		.setDescription("Bulk deletes a specified amount of messages.")
+		.addStringOption((input) =>
+			input
+				.setName("count")
+				.setDescription(
+					"The number of messages to delete, or a message ID within the last 100 messages to delete up to.",
+				)
+				.setRequired(true),
+		)
+		.addUserOption((input) =>
+			input.setName("user").setDescription("Only delete messages from this user."),
+		)
+		.setDefaultPermission(false),
+
+	async interaction(interaction) {
+		if (!interaction.channel || interaction.channel?.type === "DM")
+			throw new TypeError("Can not run this command in a DM.");
+
+		const count = interaction.options.getString("count") ?? "";
+		const user = interaction.options.getUser("user");
+		const numberCount = +count;
+		const messages = await interaction.channel.messages.fetch({ limit: 100 });
+
+		if (isNaN(numberCount) || numberCount > 100) {
+			const deleteTo = Object.keys(Object.fromEntries([...messages])).indexOf(count) + 1;
+			if (!deleteTo) {
+				return await interaction.reply(
+					`${CONSTANTS.emojis.statuses.no} Could not find a message with that ID!`,
+				);
+			}
+			return await interaction.reply(
+				await deleteMessages(messages, interaction.channel, deleteTo, user),
+			);
+		}
+		return await interaction.reply(
+			await deleteMessages(messages, interaction.channel, numberCount, user),
+		);
+	},
+};
+
+export default info;
+
+/**
+ * @param {import("discord.js").Collection<string, import("discord.js").Message<boolean>>} unfiltered
+ * @param {import("discord.js").GuildTextBasedChannel} channel
+ * @param {number} count
+ * @param {null | import("discord.js").User} user
+ */
+async function deleteMessages(unfiltered, channel, count, user) {
+	const twoWeeksAgo = Date.now() - 1_209_600_000;
+	const filtered = unfiltered
+		.toJSON()
+		.filter(
+			(message, index) =>
+				index < count &&
+				(user ? message.author.id === user.id : true) &&
+				+message.createdAt > twoWeeksAgo,
+		);
+	if (filtered.length > 0) {
+		await channel.bulkDelete(filtered);
+		return `${CONSTANTS.emojis.statuses.yes} Deleted ${filtered.length} messages!`;
+	}
+	return `${CONSTANTS.emojis.statuses.no} No messages matched those filters!`;
+}
