04e4522f2b10aedfec3f0692a3dd6890a7587acd
RedGuy12 <paul@reid-family.org>
2022-08-09 13:20:40 -0500

    Ignore mods in mass ping
    Signed-off-by: RedGuy12 <paul@reid-family.org>

--- events/message/create.js
+++ events/message/create.js
@@ -1,4 +1,7 @@
-/** @file Run Actions on posted messages. Send modmails, autoreact if contains certain triggers, and autoreply if contains certain triggers. */
+/**
+ * @file Run Actions on posted messages. Send modmails, autoreact if contains certain triggers, and
+ *   autoreply if contains certain triggers.
+ */
 import { GuildMember, Util } from "discord.js";
 import { Embed } from "@discordjs/builders";
 import CONSTANTS from "../../common/CONSTANTS.js";
@@ -162,7 +165,11 @@ const event = {
 				: message.mentions.users
 		).size;
 
-		if (mentions > 4 && message.member) {
+		if (
+			mentions > 4 &&
+			message.member &&
+			!message.member.roles.resolve(process.env.MODERATOR_ROLE || "")
+		) {
 			promises.push(
 				warn(
 					message.member,
