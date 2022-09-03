c0e342035adae409e2e520847c8781799b7ba1d7
RedGuy12 <paul@reid-family.org>
2022-09-03 09:16:12 -0500

    log applicationCommandPermissionsUpdate
    Signed-off-by: RedGuy12 <paul@reid-family.org>

---
+++ events/application/command/permissions/update.js
@@ -0,0 +1,8 @@
+import log from "../../../../common/moderation/logging.js";
+
+/** @type {import("../../../../types/event").default<"applicationCommandPermissionsUpdate">} */
+export default async function event(permissions) {
+	if (permissions.guildId !== process.env.GUILD_ID) return;
+
+	await log(`✏ Permissions for <@${permissions.applicationId}>'s commands edited!`, "server");
+}
