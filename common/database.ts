09ca7ea33e532cd0b9045e905578d7255bad8838
RedGuy12 <61329810+RedGuy12@users.noreply.github.com>
2022-09-05 11:08:27 -0500

    convert db to ts
    Signed-off-by: RedGuy12 <61329810+RedGuy12@users.noreply.github.com>

--- common/database.ts
+++ common/database.ts
@@ -1,4 +1,4 @@
-import { Message, AttachmentBuilder } from "discord.js";
+import { Message, AttachmentBuilder, Snowflake } from "discord.js";
 import papaparse from "papaparse";
 import fetch from "node-fetch";
 import exitHook from "async-exit-hook";
@@ -9,8 +9,7 @@ export const DATABASE_THREAD = "databases";
 
 const thread = await getLoggingThread(DATABASE_THREAD);
 
-/** @type {{ [key: string]: undefined | Message<true> }} */
-const databases = {};
+const databases: { [key: string]: undefined | Message<true> } = {};
 
 for (const message of (await thread.messages.fetch({ limit: 100 })).toJSON()) {
 	const name = message.content.split(" ")[1]?.toLowerCase();
@@ -19,34 +18,28 @@ for (const message of (await thread.messages.fetch({ limit: 100 })).toJSON()) {
 	}
 }
 
-/**
- * @type {{
- * 	[key: import("discord.js").Snowflake]:
- * 		| { callback: () => Promise<import("discord.js").Message<true>>; timeout: NodeJS.Timeout }
- * 		| undefined;
- * }}
- */
-const timeouts = {};
-/** @type {(keyof import("./types/databases").default)[]} */
-const contructed = [];
+const timeouts: {
+	[key: Snowflake]:
+		| { callback: () => Promise<Message<true>>; timeout: NodeJS.Timeout }
+		| undefined;
+} = {};
 
-/** @template {keyof import("./types/databases").default} Name */
-export default class Database {
-	/** @param {Name} name */
-	constructor(name) {
+const contructed: (keyof Databases)[] = [];
+
+export default class Database<Name extends keyof Databases> {
+	name: Name;
+	message: Message<true> | undefined;
+	#data: Databases[Name][] | undefined;
+	constructor(name: Name) {
 		if (contructed.includes(name))
 			throw new RangeError(
 				`Cannot create a 2nd database for ${name}, they will have conflicting data`,
 			);
 		contructed.push(name);
-		/** @type {Name} */
 		this.name = name;
 	}
 
-	/** @type {import("./types/databases").default[Name][] | undefined} */
-	#data = undefined;
 	async init() {
-		/** @type {Message<true> | undefined} */
 		this.message = databases[this.name] ||= await thread.send(
 			`**__SCRADD ${this.name.toUpperCase()} DATABASES__**\n\n*Please don’t delete this message. If you do, all ${this.name.replaceAll(
 				"_",
@@ -61,12 +54,10 @@ export default class Database {
 					.then((res) => res.text())
 					.then(
 						(csv) =>
-							/** @type {import("./types/databases").default[Name][]} */ (
-								papaparse.parse(csv.trim(), {
-									dynamicTyping: true,
-									header: true,
-								}).data
-							),
+							papaparse.parse<Databases[Name]>(csv.trim(), {
+								dynamicTyping: true,
+								header: true,
+							}).data,
 					)
 			: [];
 	}
@@ -88,8 +79,7 @@ export default class Database {
 			  ]
 			: [];
 
-		/** @returns {Promise<Message<true>>} */
-		const callback = () => {
+		const callback = (): Promise<Message<true>> => {
 			if (!this.message)
 				throw new ReferenceError("Must call `.init()` before setting `.data`");
 			const promise = this.message.edit({ files }).catch(async () => {
@@ -110,3 +100,58 @@ export function cleanDatabaseListeners() {
 }
 
 exitHook((callback) => cleanDatabaseListeners().then(callback));
+
+export type Databases = {
+	board: {
+		/** The number of reactions this message has. */
+		reactions: number;
+		/** The ID of the user who posted this. */
+		user: Snowflake;
+		/** The ID of the channel this message is in. */
+		channel: Snowflake;
+		/** The ID of the message on the board. */
+		onBoard?: Snowflake;
+		/** The ID of the original message. */
+		source: Snowflake;
+	};
+	warn: {
+		/** The ID of the user who was warned. */
+		user: Snowflake;
+		/** The timestamp when this warn expires. */
+		expiresAt: number;
+		/** The ID of the message in #mod-log with more information. */
+		info: Snowflake;
+	};
+	mute: {
+		/** The ID of the user who was muted. */
+		user: Snowflake;
+		/** The timestamp when this mute is no longer taken into account when calculating future mute times. */
+		expiresAt: number;
+	};
+	xp: {
+		/** The ID of the user. */
+		user: Snowflake;
+		/** How much XP they have. */
+		xp: number;
+	};
+	user_settings: {
+		/** The ID of the user. */
+		user: Snowflake;
+		/** Whether to ping the user when their message gets on the board. */
+		boardPings: boolean;
+		/** Whether to ping the user when they level up. */
+		levelUpPings: boolean;
+		/** Whether to ping the user when they are a top poster of the week. */
+		weeklyPings: boolean;
+		/** Whether to automatically react to their messages with random emojis. */
+		autoreactions: boolean;
+	};
+	recent_xp: {
+		/** The ID of the user. */
+		user: Snowflake;
+		/** How much XP they gained. */
+		xp: number;
+		/** The timestamp when they gained it at. */
+		timestamp: number;
+	};
+};
