f01fff52b2cadd10ad455aa33064ae2ac007bfb4
RedGuy12 <61329810+RedGuy12@users.noreply.github.com>
2022-09-06 16:17:37 -0500

    fix most xp record edge case throw on dev
    Signed-off-by: RedGuy12 <61329810+RedGuy12@users.noreply.github.com>

--- common/xp.js
+++ common/xp.js
@@ -173,9 +173,12 @@ export default async function giveXp(to, amount = NORMAL_XP_PER_MESSAGE) {
 		await breakRecord(2, [await client.users.fetch(topInHour[0])], topInHour[1], thread);
 
 	const users = Object.keys(hourlyByUser);
+
 	await breakRecord(
 		6,
-		await Promise.all(users.map((user) => client.users.fetch(user))),
+		(
+			await Promise.all(users.map((user) => client.users.fetch(user).catch(() => {})))
+		).filter(/** @returns {user is NonNullable<typeof user>} */ (user) => !!user),
 		users.length,
 	);
 }
