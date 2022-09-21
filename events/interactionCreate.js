5073283449703c127c6d6982dbe1bc0a7a834597
RedGuy12 <61329810+RedGuy12@users.noreply.github.com>
2022-09-21 09:48:26 -0500

    dont overwrite og responses on error
    Signed-off-by: RedGuy12 <61329810+RedGuy12@users.noreply.github.com>

--- events/interactionCreate.js
+++ events/interactionCreate.js
@@ -107,7 +107,9 @@ export default async function event(interaction) {
 		await command.interaction(interaction);
 	} catch (error) {
 		logError(error, interaction.toString());
-		await interaction[interaction.replied || interaction.deferred ? "reply" : "editReply"]({
+		await interaction[
+			interaction.replied ? "followUp" : interaction.deferred ? "editReply" : "reply"
+		]({
 			ephemeral: true,
 			content: `${CONSTANTS.emojis.statuses.no} An error occurred.`,
 			embeds: [],
