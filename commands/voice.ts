962b554f25c5a153e4630fafad889f0da5775b0e
RedGuy12 <paul@reid-family.org>
2022-10-07 17:23:54 -0500

    finish /voice sound
    Signed-off-by: RedGuy12 <paul@reid-family.org>

--- commands/voice.ts
+++ commands/voice.ts
@@ -15,15 +15,14 @@ import {
 	createAudioPlayer,
 	NoSubscriberBehavior,
 	createAudioResource,
-	StreamType,
-	// AudioPlayerStatus,
+	AudioPlayerStatus,
 } from "@discordjs/voice";
 import type { ChatInputCommand } from "../common/types/command.js";
 import { guild } from "../client.js";
-import { createReadStream } from "fs";
 import url from "node:url";
 import path from "path";
 import CONSTANTS from "../common/CONSTANTS.js";
+import log from "../common/moderation/logging.js";
 
 const info: ChatInputCommand = {
 	data: new SlashCommandBuilder()
@@ -61,41 +60,104 @@ const info: ChatInputCommand = {
 						.setDescription("The channel to start the activity in")
 						.addChannelTypes(ChannelType.GuildVoice),
 				),
+		)
+		.addSubcommand((input) =>
+			input
+				.setName("meme-sound")
+				.setDescription("Play a meme sound")
+				.addStringOption((input) =>
+					input
+						.setName("sound")
+						.setDescription("The sound to play")
+						.setRequired(true)
+						.addChoices(
+							{ name: "AMONGUS", value: "amongus.mp3" },
+							{ name: "And thank you!", value: "youreWelcome.mp3" },
+							{
+								name: "Big Big Chungus (big chungus, big chungus)",
+								value: "bigChungus.mp3",
+							},
+							{ name: "breuh", value: "breh.mp3" },
+							{ name: "Crab Rave", value: "crabRave.mp3" },
+							{ name: "Creeper? Awww, man", value: "creeper.mp3" },
+							{ name: "E", value: "e.mp3" },
+							{ name: "E-mo-tion-al dam-age", value: "emotionalDamage.mp3" },
+							{ name: "FBI OPEN UP", value: "fbi.mp3" },
+							{ name: "Innocent song", value: "rickroll.mp3" },
+							{ name: "Ladies and Gentlemen, we got 'em!", value: "weGotEm.mp3" },
+							{ name: "Megalovania", value: "megalovania.mp3" },
+							{ name: "NANI?!", value: "nani.mp3" },
+							{
+								name: "Now that's a lotta damage!",
+								value: "nowThatsALottaDamage.m4a",
+							},
+							{ name: "oof", value: "oof.mp3" },
+							{ name: "Ping!1!!1!!!", value: "discord.mp3" },
+							{ name: "Somebody once told me", value: "allStar.mp3" },
+							{ name: "Taco Bell", value: "bong.mp3" },
+							{ name: "This group is dead", value: "dead.mp3" },
+							{ name: "WAS THAT THE BITE OF 87?!?!?", value: "biteOf87.wav" },
+							{ name: "Wii", value: "wii.mp3" },
+							{ name: "Wut da dog doin?", value: "wutDaDogDoin.mp3" },
+							{ name: "YEEEET!", value: "yeet.mp3" },
+							{ name: "Yoshi", value: "yoshi.mp3" },
+						),
+				)
+				.addChannelOption((input) =>
+					input
+						.setName("channel")
+						.setDescription("The channel to play the sound in")
+						.addChannelTypes(ChannelType.GuildVoice),
+				),
+		)
+		.addSubcommand((input) =>
+			input
+				.setName("quote-sound")
+				.setDescription("Play a quote sound")
+				.addStringOption((input) =>
+					input
+						.setName("sound")
+						.setDescription("The sound to play")
+						.setRequired(true)
+						.addChoices(
+							{
+								name: "And then, Colaber, voice revealed",
+								value: "colaberVoiceReveal.wav",
+							},
+							{ name: "Co-Lay-Burrrr", value: "squidward.mp3" },
+							{ name: "ColaberColaberColaber", value: "colaber.mp3" },
+							{ name: "I Feel Good", value: "iFeelGood.wav" },
+							{ name: "nom nom nom", value: "chips.mp3" },
+							{ name: "Not Today, Griffpatch", value: "notTodayGriffpatch.mp3" },
+							{ name: "poe-tah-toe", value: "potato.mp3" },
+							{ name: "Scratch Notifier", value: "ping.mp3" },
+							{
+								name: "The World's Smallest Violin",
+								value: "worldsSmallestViolin.wav",
+							},
+							{ name: "This Is Scratch Addons.", value: "thisIs.mp3" },
+							{
+								name: "We gotta get Red Guy back!",
+								value: "weGottaGetRedGuyBack.mp3",
+							},
+							{ name: "TED Talk", value: "tedTalk.wav" },
+							{
+								name: "We've got some REALLY good content today, though.",
+								value: "balls.wav",
+							},
+							{
+								name: "Welcome back to the Scratch Addons YouTube channel",
+								value: "welcomeBack.mp3",
+							},
+						),
+				)
+				.addChannelOption((input) =>
+					input
+						.setName("channel")
+						.setDescription("The channel to play the sound in")
+						.addChannelTypes(ChannelType.GuildVoice),
+				),
 		),
-	// .addSubcommand((input) =>
-	// 	input
-	// 		.setName("sound")
-	// 		.setDescription("Play a sound")
-	// 		.addStringOption((input) =>
-	// 			input
-	// 				.setName("sound")
-	// 				.setDescription("The sound to play")
-	// 				.setRequired(true)
-	// 				.addChoices(
-	// 					{ name: "Poker Night", value: "755827207812677713" },
-	// 					{ name: "Chess in the Park", value: "832012774040141894" },
-	// 					{ name: "Checkers in the Park", value: "832013003968348200" },
-	// 					{ name: "Blazing 8s", value: "832025144389533716" },
-	// 					{ name: "Watch Together", value: "880218394199220334" },
-	// 					{ name: "Letter League", value: "879863686565621790" },
-	// 					{ name: "Word Snacks", value: "879863976006127627" },
-	// 					{ name: "Sketch Heads", value: "902271654783242291" },
-	// 					{ name: "SpellCast", value: "852509694341283871" },
-	// 					{ name: "Land-io", value: "903769130790969345" },
-	// 					{ name: "Putt Party", value: "945737671223947305" },
-	// 					{ name: "Bobble League", value: "947957217959759964" },
-	// 					{ name: "Know What I Meme", value: "950505761862189096" },
-	// 					{ name: "AskAway", value: "976052223358406656" },
-	// 					{ name: "Bash Out", value: "1006584476094177371" },
-	// 				),
-	// 		)
-	// 		.addChannelOption((input) =>
-	// 			input
-	// 				.setName("channel")
-	// 				.setDescription("The channel to play the sound in")
-	// 				.addChannelTypes(ChannelType.GuildVoice),
-	// 		),
-	// )
 
 	async interaction(interaction) {
 		if (!(interaction.member instanceof GuildMember))
@@ -133,7 +195,8 @@ const info: ChatInputCommand = {
 					ephemeral: true,
 				});
 			}
-			case "sound": {
+			case "meme-sound":
+			case "quote-sound": {
 				if (guild.members.me?.voice.channel)
 					return interaction.reply({
 						ephemeral: true,
@@ -153,30 +216,19 @@ const info: ChatInputCommand = {
 					connection.disconnect();
 					throw error;
 				});
+				connection.subscribe(player);
+				player.play(
+					createAudioResource(
+						path.resolve(
+							path.dirname(url.fileURLToPath(import.meta.url)),
+							`../../common/audio/${interaction.options.getString("sound", true)}`,
+						),
+					),
+				);
+				player.on(AudioPlayerStatus.Idle, () => connection.disconnect());
 
-				console.log("inited");
 				connection
-					.on(VoiceConnectionStatus.Ready, () => {
-						console.log("ready");
-
-						connection.subscribe(player);
-						console.log("subscribed");
-						player.play(
-							createAudioResource(
-								createReadStream(
-									path.resolve(
-										path.dirname(url.fileURLToPath(import.meta.url)),
-										"../../common/audio/biteOf87.ogg",
-									),
-								),
-								{ inputType: StreamType.OggOpus },
-							),
-						);
-						console.log("playing");
-						// player.on(AudioPlayerStatus.Idle, connection.disconnect);
-					})
 					.on(VoiceConnectionStatus.Disconnected, async () => {
-						console.log("disconnect?");
 						try {
 							await Promise.race([
 								entersState(connection, VoiceConnectionStatus.Signalling, 5_000),
@@ -184,18 +236,26 @@ const info: ChatInputCommand = {
 							]);
 							// Seems to be reconnecting to a new channel - ignore disconnect
 						} catch {
-							console.log("disconnect");
 							// Seems to be a real disconnect which SHOULDN'T be recovered from
 							connection.destroy();
 							player.stop();
 						}
 					})
 					.on("error", (error) => {
-						connection.disconnect();
+						player.stop();
 						throw error;
 					});
+
+				await Promise.all([
+					interaction.reply(CONSTANTS.emojis.statuses.yes),
+					log(
+						`🎤 ${interaction.user.toString()} used \`${interaction?.toString()}\` in ${channel.toString()}!`,
+						"voice",
+					),
+				]);
 			}
 		}
 	},
 };
+
 export default info;
--- events/voiceStateUpdate.js
+++ events/voiceStateUpdate.js
@@ -1,4 +1,3 @@
-import CONSTANTS from "../common/CONSTANTS.js";
 import log from "../common/moderation/logging.js";
 
 /** @type {import("../common/types/event").default<"voiceStateUpdate">} */
@@ -6,7 +5,7 @@ export default async function event(oldState, newState) {
 	if (!newState.member || newState.guild.id !== process.env.GUILD_ID) return;
 
 	const logs = [];
-	if (oldState.channel?.id !== newState.channel?.id && newState.member.id !== CONSTANTS.robotop) {
+	if (oldState.channel?.id !== newState.channel?.id && !newState.member.user.bot) {
 		if (newState.channel) {
 			logs.push(
 				`joined voice channel ${newState.channel.toString()}, ${
@@ -18,24 +17,24 @@ export default async function event(oldState, newState) {
 			logs.push(`left voice channel ${oldState.channel.toString()}`);
 		}
 	} else if (newState.channel) {
-		if (oldState.serverMute !== newState.serverMute) {
+		if (!!oldState.serverMute !== !!newState.serverMute) {
 			logs.push(`was${newState.serverMute ? "" : " un"} server muted`);
-		} else if (oldState.mute !== newState.mute) {
+		} else if (!!oldState.mute !== !!newState.mute) {
 			logs.push(`${newState.mute ? "" : "un"}muted in ${newState.channel.toString()}`);
 		}
-		if (oldState.serverDeaf !== newState.serverDeaf) {
+		if (!!oldState.serverDeaf !== !!newState.serverDeaf) {
 			logs.push(`was${newState.serverDeaf ? "" : " un"} server deafened`);
-		} else if (oldState.deaf !== newState.deaf) {
+		} else if (!!oldState.deaf !== !!newState.deaf) {
 			logs.push(`${newState.deaf ? "" : "un"}deafened in ${newState.channel.toString()}`);
 		}
-		if (oldState.suppress !== newState.suppress) {
+		if (!!oldState.suppress !== !!newState.suppress) {
 			logs.push(
 				`${
 					newState.suppress ? "moved to the audience" : "became a speaker"
 				} in ${newState.channel.toString()}`,
 			);
 		}
-		if (oldState.streaming !== newState.streaming) {
+		if (!!oldState.streaming !== !!newState.streaming) {
 			logs.push(
 				`${
 					newState.streaming ? "started" : "stopped"
