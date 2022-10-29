2bae7b625aa753738ec7a5b9ec40c1d9bed013b7
RedGuy12 <61329810+RedGuy12@users.noreply.github.com>
2022-10-29 12:38:20 -0500

    A

--- common/xp.js
+++ common/xp.js
@@ -12,6 +12,7 @@ await weeklyXpDatabase.init();
 
 export const NORMAL_XP_PER_MESSAGE = 5;
 
+/** @param {any[]} args */
 export default async function abc(...args) {return args}
 /** @param {User | GuildMember} to */
 export async function giveXp(to, amount = NORMAL_XP_PER_MESSAGE) {
