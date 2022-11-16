0cbb18321db430793684333033bd38fc4611c0cb
RedGuy12 <paul@reid-family.org>
2022-11-15 22:24:38 -0600

    Add emoji to the bad word in status dm
    Signed-off-by: RedGuy12 <paul@reid-family.org>

--- events/presenceUpdate.ts
+++ events/presenceUpdate.ts
@@ -18,7 +18,7 @@ const event: Event<"presenceUpdate"> = async function event(_, newPresence) {
 		newPresence.member?.roles.resolve(CONSTANTS.roles.mod.id)
 	) {
 		await member?.send(
-			"As a mod, you should set an example for the server, so please refrain from swears in your status. Thanks!",
+			"⚠ As a mod, you should set an example for the server, so please refrain from swears in your status. Thanks!",
 		);
 	}
 };
