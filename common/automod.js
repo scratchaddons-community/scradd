5f94fd88a1fc965064bb04560ef424a2cc3c1752
RedGuy12 <RedGuy12@users.noreply.github.com>
2022-11-27 21:53:16 +0000

    Format

--- common/automod.js
+++ common/automod.js
@@ -209,12 +209,13 @@ async function checkString(toCensor, message) {
 		}
 	}
 
-const baseChannel=getBaseChannel(message.channel)
-const parentChannel = baseChannel&&baseChannel.isDMBased()?baseChannel:baseChannel?.parent
+	const baseChannel = getBaseChannel(message.channel);
+	const parentChannel =
+		baseChannel && baseChannel.isDMBased() ? baseChannel : baseChannel?.parent;
 
 	if (
 		!badWordsAllowed(message.channel) &&
-		(CONSTANTS.channels.info?.id !== parentChannel?.id) &&
+		CONSTANTS.channels.info?.id !== parentChannel?.id &&
 		!message.author?.bot
 	) {
 		const botLinks = toCensor.match(/discord(?:app)?\.com\/(api\/)?oauth2\/authorize/gi);
