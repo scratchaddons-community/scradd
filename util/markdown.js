769cfc8d9125ff99aeab7b9b0643be04439e2b6c
RedGuy12 <paul@reid-family.org>
2022-12-16 08:33:01 -0600

    ah yes
    Signed-off-by: RedGuy12 <paul@reid-family.org>

--- util/markdown.js
+++ util/markdown.js
@@ -17,7 +17,8 @@ export function escapeMessage(text) {
  * @param {string} text - The text to escape.
  *
  * @returns {string} The escaped text.
- * @todo djs has this
+ *
+ * @todo Djs has this.
  */
 export function escapeLinks(text) {
 	while (text.split("[").length > text.split("]").length) text = text.replace("[", "");
