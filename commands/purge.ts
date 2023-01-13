aee0546eeeffbfd1eabc941d4c155b9b10b17c16
RedGuy12 <61329810+RedGuy12@users.noreply.github.com>
2023-01-13 11:34:25 -0600

    Rewrite /purge
    Signed-off-by: RedGuy12 <61329810+RedGuy12@users.noreply.github.com>

--- commands/purge.ts
+++ commands/purge.ts
@@ -1,50 +1,17 @@
 import {
+	APIEmbed,
 	ApplicationCommandOptionType,
-	type Collection,
-	type Message,
-	type Snowflake,
-	type User,
+	ButtonStyle,
+	ComponentType,
+	InteractionReplyOptions,
 } from "discord.js";
+import client from "../client.js";
 
 import CONSTANTS from "../common/CONSTANTS.js";
 import { defineCommand } from "../common/types/command.js";
+import { disableComponents, messageToText } from "../util/discord.js";
 
 const MAX_FETCH_COUNT = 100;
-
-/**
- * Filter and bulk delete messages.
- *
- * @param unfiltered - The messages to be deleted.
- * @param count - How many messages to delete (inclusive).
- * @param user - Filter to only delete messages from this user.
- */
-async function deleteMessages(
-	unfiltered: Collection<Snowflake, Message<true>>,
-	count: number,
-	user?: User,
-) {
-	const filtered = unfiltered
-		.toJSON()
-		.filter(
-			(message, index) =>
-				index < count &&
-				(user ? message.author.id === user.id : true) &&
-				message.bulkDeletable,
-		);
-	if (filtered[0]) {
-		// channel.createMessageComponentCollector({});
-		// return {};
-		await filtered[0].channel.bulkDelete(filtered);
-		return {
-			content: `${CONSTANTS.emojis.statuses.yes} Deleted ${filtered.length} messages!`,
-			ephemeral: true,
-		};
-	}
-	return {
-		content: `${CONSTANTS.emojis.statuses.no} No messages matched those filters! Note: I cannot delete messages older than 2 weeks or more than ${MAX_FETCH_COUNT} messages at a time.`,
-		ephemeral: true,
-	};
-}
 
 const command = defineCommand({
 	data: {
@@ -55,14 +22,20 @@ const command = defineCommand({
 				type: ApplicationCommandOptionType.String,
 
 				description:
-					"The number of messages to delete or a message ID to delete to (inclusive)",
+					"The number of messages to purge or a message ID to delete to (inclusive)",
 
 				required: true,
 			},
 
 			user: {
 				type: ApplicationCommandOptionType.User,
-				description: "Only delete messages from this user",
+				description: "Only purge messages from this user",
+			},
+
+			message: {
+				type: ApplicationCommandOptionType.String,
+
+				description: "The message ID to count backwards from (exclusive).",
 			},
 		},
 
@@ -70,24 +43,222 @@ const command = defineCommand({
 	},
 
 	async interaction(interaction) {
-		if (!interaction.channel) throw new TypeError("Cannot run this command in a DM");
-
 		const count = interaction.options.getString("count", true);
 		const user = interaction.options.getUser("user") ?? undefined;
+		const message =
+			interaction.options.getString("message")?.match(/^(?:\d+-)?(?<id>\d+)$/)?.groups?.id ??
+			undefined;
 		const numberCount = Number(count);
-		const messages = await interaction.channel.messages.fetch({ limit: MAX_FETCH_COUNT });
+		const useId = Number.isNaN(numberCount) || numberCount > MAX_FETCH_COUNT;
+		const { channel: channelId, id: countId } = (useId &&
+			count.match(/^(?:(?<channel>\d+)-)?(?<id>\d+)$/)?.groups) || { id: count };
+		const channel = channelId ? await client.channels.fetch(channelId) : interaction.channel;
+		if (!channel?.isTextBased() || channel.isDMBased())
+			return await interaction.reply(
+				`${CONSTANTS.emojis.statuses.no} Could not find that channel!`,
+			);
+		const messages = await channel.messages.fetch({ limit: MAX_FETCH_COUNT, before: message });
 
-		if (Number.isNaN(numberCount) || numberCount > MAX_FETCH_COUNT) {
-			const deleteTo = Object.keys(Object.fromEntries(messages)).indexOf(count) + 1;
-			await (deleteTo
-				? interaction.reply(await deleteMessages(messages, deleteTo, user))
-				: interaction.reply({
-						content: `${CONSTANTS.emojis.statuses.no} Could not find a message with that ID! Note: I cannot delete messages older than 2 weeks or more than ${MAX_FETCH_COUNT} messages at a time.`,
-						ephemeral: true,
-				  }));
-		} else {
-			await interaction.reply(await deleteMessages(messages, numberCount, user));
+		const filtered = messages
+			.toJSON()
+			.filter(
+				(message) => (user ? message.author.id === user.id : true) && message.bulkDeletable,
+			);
+
+		let start = 0;
+
+		let deleteTo = useId ? filtered.findIndex(({ id }) => id === countId) + 1 : numberCount;
+
+		async function generateMessage() {
+			const sliced = filtered.slice(start, deleteTo);
+			if (!sliced[0] || deleteTo > filtered.length || start >= deleteTo) {
+				return {
+					content: `${
+						CONSTANTS.emojis.statuses.no
+					} No messages matched those filters! Note: I cannot purge messages that are older than 2 weeks or more than ${MAX_FETCH_COUNT} messages ${
+						message ? `before [this message](${channel?.url}/${message})` : "ago"
+					}.`,
+					ephemeral: true,
+				};
+			}
+
+			const embeds: APIEmbed[] = [];
+
+			const last = sliced.at(-1);
+			if (sliced.length > 1 && last) {
+				embeds.push({
+					color: last.member?.displayColor,
+					description: await messageToText(last),
+
+					author: {
+						icon_url: (last.member ?? last.author).displayAvatarURL(),
+						name: last.member?.displayName ?? last.author.username,
+					},
+
+					timestamp: last.createdAt.toISOString(),
+				});
+			}
+
+			if (sliced.length > 2)
+				embeds.push({
+					description: `*${sliced.length - 2} more message${
+						sliced.length - 2 === 1 ? "" : "s"
+					}…*`,
+				});
+
+			embeds.push({
+				color: sliced[0].member?.displayColor,
+				description: await messageToText(sliced[0]),
+
+				author: {
+					icon_url: (sliced[0].member ?? sliced[0].author).displayAvatarURL(),
+					name: sliced[0].member?.displayName ?? sliced[0].author.username,
+				},
+
+				timestamp: sliced[0].createdAt.toISOString(),
+			});
+
+			return {
+				ephemeral: true,
+				content: `Are you sure you want to purge th${
+					sliced.length === 1 ? `is message` : `ese ${sliced.length} messages`
+				}?`,
+				embeds,
+				components: [
+					{
+						type: ComponentType.ActionRow,
+						components: [
+							{
+								type: ComponentType.Button,
+								style: ButtonStyle.Link,
+								label: "First Message",
+								url: (last || sliced[0]).url,
+							},
+							{
+								type: ComponentType.Button,
+								customId: `first-remove-${interaction.id}`,
+								style: ButtonStyle.Primary,
+								disabled: start >= deleteTo - 1,
+								label: "Remove First",
+							},
+							{
+								type: ComponentType.Button,
+								customId: `first-add-${interaction.id}`,
+								style: ButtonStyle.Primary,
+								disabled: deleteTo >= filtered.length,
+								label: "Prepend One",
+							},
+						],
+					},
+					{
+						type: ComponentType.ActionRow,
+						components: [
+							{
+								type: ComponentType.Button,
+								style: ButtonStyle.Link,
+								label: "Last Message",
+								url: sliced[0].url,
+							},
+							{
+								type: ComponentType.Button,
+								customId: `last-remove-${interaction.id}`,
+								style: ButtonStyle.Primary,
+								disabled: start >= deleteTo - 1,
+								label: "Remove Last",
+							},
+							{
+								type: ComponentType.Button,
+								customId: `last-add-${interaction.id}`,
+								style: ButtonStyle.Primary,
+								disabled: start < 1,
+								label: "Append One",
+							},
+						],
+					},
+					{
+						type: ComponentType.ActionRow,
+						components: [
+							{
+								type: ComponentType.Button,
+								label: "Confirm",
+								style: ButtonStyle.Success,
+								customId: `confirm-${interaction.id}`,
+							},
+							{
+								type: ComponentType.Button,
+								label: "Cancel",
+								customId: `cancel-${interaction.id}`,
+								style: ButtonStyle.Danger,
+							},
+						],
+					},
+				],
+				fetchReply: true,
+			} satisfies InteractionReplyOptions;
 		}
+
+		const generated = await generateMessage();
+		await interaction.reply(generated);
+		if (!generated.fetchReply) return;
+
+		const collector = interaction.channel?.createMessageComponentCollector({
+			filter: (buttonInteraction) => {
+				return buttonInteraction.customId.endsWith(`-${interaction.id}`);
+			},
+
+			time: CONSTANTS.collectorTime,
+		});
+
+		collector
+			?.on("collect", async (buttonInteraction) => {
+				const split = buttonInteraction.customId.split("-");
+				split.pop();
+
+				switch (split[0]) {
+					case "confirm": {
+						const sliced = filtered.slice(start, deleteTo);
+						await channel?.bulkDelete(sliced);
+						await buttonInteraction.reply({
+							content: `${CONSTANTS.emojis.statuses.yes} Purged ${
+								sliced.length
+							} message${sliced.length === 1 ? "" : "s"}!`,
+							ephemeral: true,
+						});
+						collector.stop();
+						return;
+					}
+					case "cancel": {
+						await buttonInteraction.reply({
+							content: `${CONSTANTS.emojis.statuses.no} Purge canceled!`,
+							ephemeral: true,
+						});
+						collector.stop();
+						return;
+					}
+					case "first": {
+						if (split[1] === "remove") deleteTo--;
+						else deleteTo++;
+						break;
+					}
+					case "last": {
+						if (split[1] === "remove") start++;
+						else start--;
+					}
+				}
+
+				await buttonInteraction.deferUpdate();
+
+				const generated = await generateMessage();
+				await interaction.editReply(generated);
+
+				if (!generated.fetchReply) collector.stop();
+				else collector.resetTimer();
+			})
+			.on("end", async () => {
+				await interaction.editReply({
+					components: disableComponents((await interaction.fetchReply()).components),
+				});
+			});
 	},
 });
 export default command;
