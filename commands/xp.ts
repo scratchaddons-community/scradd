708eb2ca34c4aa3a6b1a9ec7408ae3a9d77eb5f9
RedGuy12 <paul@reid-family.org>
2023-03-09 20:38:20 -0600

    /xp rank: Add %
    Signed-off-by: RedGuy12 <paul@reid-family.org>

--- commands/xp.ts
+++ commands/xp.ts
@@ -1,5 +1,12 @@
-import { createCanvas } from "@napi-rs/canvas";
-import { ApplicationCommandOptionType } from "discord.js";
+import { createCanvas, GlobalFonts } from "@napi-rs/canvas";
+import {
+	ApplicationCommandOptionType,
+	ButtonInteraction,
+	ChatInputCommandInteraction,
+	User,
+} from "discord.js";
+import path from "node:path";
+import url from "node:url";
 
 import client from "../client.js";
 import CONSTANTS from "../common/CONSTANTS.js";
@@ -13,6 +20,11 @@ import {
 import { paginate } from "../util/discord.js";
 import { convertBase, nth } from "../util/numbers.js";
 import { userSettingsDatabase } from "./settings.js";
+
+GlobalFonts.registerFromPath(
+	path.resolve(path.dirname(url.fileURLToPath(import.meta.url)), `../../common/sora/font.ttf`),
+	"Sora",
+);
 
 const command = defineCommand({
 	data: {
@@ -46,112 +58,16 @@ const command = defineCommand({
 	async interaction(interaction) {
 		const command = interaction.options.getSubcommand(true);
 
-		const allXp = database.data;
-		const top = allXp.sort((one, two) => two.xp - one.xp);
-
 		switch (command) {
 			case "rank": {
 				const user = interaction.options.getUser("user") ?? interaction.user;
-
-				const member = await CONSTANTS.guild.members.fetch(user.id).catch(() => {});
-
-				const xp = Math.floor(allXp.find((entry) => entry.user === user.id)?.xp ?? 0);
-				const level = getLevelForXp(xp);
-				const xpForNextLevel = getXpForLevel(level + 1);
-				const xpForPreviousLevel = getXpForLevel(level);
-				const increment = xpForNextLevel - xpForPreviousLevel;
-				const xpGained = xp - xpForPreviousLevel;
-				const progress = xpGained / increment;
-				const rank = top.findIndex((info) => info.user === user.id) + 1;
-				const weeklyRank =
-					weeklyXpDatabase.data
-						.sort((one, two) => two.xp - one.xp)
-						.findIndex((entry) => entry.user === user.id) + 1;
-				const approximateWeeklyRank = Math.ceil(weeklyRank / 10) * 10;
-
-				const canvas = createCanvas(1000, 50);
-				const context = canvas.getContext("2d");
-				context.fillStyle = `#${convertBase(String(CONSTANTS.themeColor), 10, 16)}`;
-				const rectangleSize = canvas.width * progress;
-				const paddingPixels = 0.18 * canvas.height;
-				context.fillRect(0, 0, rectangleSize, canvas.height);
-				context.font = `${canvas.height * 0.9}px sans-serif`;
-				context.fillStyle = "#00000096";
-				if (progress < 0.145) {
-					context.textAlign = "end";
-					context.fillText(
-						progress.toLocaleString([], {
-							maximumFractionDigits: 1,
-							style: "percent",
-						}),
-						canvas.width - paddingPixels,
-						canvas.height - paddingPixels,
-					);
-				} else {
-					context.fillText(
-						progress.toLocaleString([], {
-							maximumFractionDigits: 1,
-							style: "percent",
-						}),
-						paddingPixels,
-						canvas.height - paddingPixels,
-					);
-				}
-
-				await interaction.reply({
-					embeds: [
-						{
-							color: member?.displayColor,
-
-							author: {
-								icon_url: (member || user).displayAvatarURL(),
-								name: member?.displayName ?? user.username,
-							},
-
-							title: "XP Rank",
-
-							fields: [
-								{ name: "📊 Level", value: level.toLocaleString(), inline: true },
-								{ name: "✨ XP", value: xp.toLocaleString(), inline: true },
-								{
-									name: "⏲ Weekly rank",
-
-									value: weeklyRank
-										? approximateWeeklyRank === 10
-											? "Top 10"
-											: `About ${nth(approximateWeeklyRank - 5, {
-													bold: false,
-													jokes: false,
-											  })}`
-										: "Inactive",
-
-									inline: true,
-								},
-								{
-									name: CONSTANTS.zeroWidthSpace,
-									value: `**⬆ Next level progress** ${xpForNextLevel.toLocaleString()} XP needed`,
-								},
-							],
-
-							footer: {
-								text: `${
-									rank
-										? `Ranked ${rank.toLocaleString()}/${top.length.toLocaleString()}${
-												CONSTANTS.footerSeperator
-										  }`
-										: ""
-								}View the leaderboard with /xp top`,
-							},
-
-							image: { url: "attachment://progress.png" },
-						},
-					],
-
-					files: [{ attachment: canvas.toBuffer("image/png"), name: "progress.png" }],
-				});
+				await getUserRank(interaction, user);
 				return;
 			}
 			case "top": {
+				const allXp = database.data;
+				const top = allXp.sort((one, two) => two.xp - one.xp);
+
 				const user = interaction.options.getUser("user");
 				const useMentions =
 					userSettingsDatabase.data.find(
@@ -192,5 +108,118 @@ const command = defineCommand({
 			}
 		}
 	},
+
+	buttons: {
+		async xp(interaction, userId = "") {
+			await getUserRank(interaction, await client.users.fetch(userId));
+		},
+	},
 });
 export default command;
+
+async function getUserRank(
+	interaction: ChatInputCommandInteraction<"cached" | "raw"> | ButtonInteraction,
+	user: User,
+) {
+	const allXp = database.data;
+	const top = allXp.sort((one, two) => two.xp - one.xp);
+
+	const member = await CONSTANTS.guild.members.fetch(user.id).catch(() => {});
+
+	const xp = Math.floor(allXp.find((entry) => entry.user === user.id)?.xp ?? 0);
+	const level = getLevelForXp(xp);
+	const xpForNextLevel = getXpForLevel(level + 1);
+	const xpForPreviousLevel = getXpForLevel(level);
+	const increment = xpForNextLevel - xpForPreviousLevel;
+	const xpGained = xp - xpForPreviousLevel;
+	const progress = xpGained / increment;
+	const rank = top.findIndex((info) => info.user === user.id) + 1;
+	const weeklyRank =
+		weeklyXpDatabase.data
+			.sort((one, two) => two.xp - one.xp)
+			.findIndex((entry) => entry.user === user.id) + 1;
+	const approximateWeeklyRank = Math.ceil(weeklyRank / 10) * 10;
+
+	const canvas = createCanvas(1000, 50);
+	const context = canvas.getContext("2d");
+	context.fillStyle = "#0003";
+	context.fillRect(0, 0, canvas.width, canvas.height);
+	context.fillStyle = `#${convertBase(String(CONSTANTS.themeColor), 10, 16)}`;
+	const rectangleSize = canvas.width * progress;
+	const paddingPixels = 0.18 * canvas.height;
+	context.fillRect(0, 0, rectangleSize, canvas.height);
+	context.font = `${canvas.height * 0.9}px Sora`;
+	context.fillStyle = "#000c";
+	if (progress < 0.145) {
+		context.textAlign = "end";
+		context.fillText(
+			progress.toLocaleString([], {
+				maximumFractionDigits: 1,
+				style: "percent",
+			}),
+			canvas.width - paddingPixels,
+			canvas.height - paddingPixels,
+		);
+	} else {
+		context.fillText(
+			progress.toLocaleString([], {
+				maximumFractionDigits: 1,
+				style: "percent",
+			}),
+			paddingPixels,
+			canvas.height - paddingPixels,
+		);
+	}
+
+	await interaction.reply({
+		embeds: [
+			{
+				color: member?.displayColor,
+
+				author: {
+					icon_url: (member || user).displayAvatarURL(),
+					name: member?.displayName ?? user.username,
+				},
+
+				title: "XP Rank",
+
+				fields: [
+					{ name: "📊 Level", value: level.toLocaleString(), inline: true },
+					{ name: "✨ XP", value: xp.toLocaleString(), inline: true },
+					{
+						name: "⏲ Weekly rank",
+
+						value: weeklyRank
+							? approximateWeeklyRank === 10
+								? "Top 10"
+								: `About ${nth(approximateWeeklyRank - 5, {
+										bold: false,
+										jokes: false,
+								  })}`
+							: "Inactive",
+
+						inline: true,
+					},
+					{
+						name: CONSTANTS.zeroWidthSpace,
+						value: `**⬆ Next level progress** ${xpForNextLevel.toLocaleString()} XP needed`,
+					},
+				],
+
+				footer: {
+					text: `${
+						rank
+							? `Ranked ${rank.toLocaleString()}/${top.length.toLocaleString()}${
+									CONSTANTS.footerSeperator
+							  }`
+							: ""
+					}View the leaderboard with /xp top`,
+				},
+
+				image: { url: "attachment://progress.png" },
+			},
+		],
+
+		files: [{ attachment: canvas.toBuffer("image/png"), name: "progress.png" }],
+	});
+}
