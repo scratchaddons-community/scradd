3e268d1e798655d8b4e0140f91eaa98524dc0ec1
RedGuy12 <RedGuy12@users.noreply.github.com>
2023-05-20 03:52:50 +0000

    Format

--- badWords.ts
+++ badWords.ts
@@ -1,29 +1,15 @@
 /**
  * The index of each array determines how many strikes the word gives.
  *
- * The second sub-array is for words that must be surrounded by a word boundary and the third is for words that must be preceded by a word boundary.
+ * The second sub-array is for words that must be surrounded by a word boundary and the third is for words that must be preceded by a word
+ * boundary.
  *
  * All words are ROT13-encoded.
  */
 const badWords: [RegExp[]?, RegExp[]?, RegExp[]?][] = [
-	[
-		[
-		],
-		[
-		],
-	],
-	[
-		[
-		],
-		[
-		],
-	],
-	[
-		[
-		],
-		[],
-		[],
-	],
+	[[], []],
+	[[], []],
+	[[], [], []],
 ];
 
 if (process.env.NODE_ENV !== "production") badWords[1]?.[0].push(/nhgbzbqzhgr/);
