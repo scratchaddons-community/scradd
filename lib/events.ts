d5559ca4fb26078a36121d301ef799ba5f3bb57c
RedGuy12 <paul@reid-family.org>
2023-06-07 17:38:15 -0500

    better error handling
    Signed-off-by: RedGuy12 <paul@reid-family.org>

--- lib/events.ts
+++ lib/events.ts
@@ -9,22 +9,15 @@ type ReservedClientEvent =
 	| "guildUnavailable";
 export type ClientEvent = Exclude<keyof ClientEvents, ReservedClientEvent>;
 export type Event = (...args: ClientEvents[ClientEvent]) => unknown;
-const events: Record<string, Event> = {};
+const allEvents: Record<string, Event[]> = {};
 const preEvents: Record<string, Event> = {};
 
 export default function defineEvent<EventName extends ClientEvent>(
 	eventName: EventName,
 	event: (...args: ClientEvents[EventName]) => unknown,
 ) {
-	const old = events[eventName];
-	if (old) {
-		events[eventName] = async function (...args: ClientEvents[EventName]) {
-			old(...args);
-			event(...args);
-		} as Event;
-	} else {
-		events[eventName] = event as Event;
-	}
+	allEvents[eventName] ??= [];
+	allEvents[eventName]?.push(event as Event);
 }
 defineEvent.pre = function pre<EventName extends ClientEvent>(
 	eventName: EventName,
@@ -33,20 +26,32 @@ defineEvent.pre = function pre<EventName extends ClientEvent>(
 	if (preEvents[eventName])
 		throw new ReferenceError("Pre event for event " + eventName + " already exists");
 	preEvents[eventName] = event as Event;
+	allEvents[eventName] ??= [];
 };
 
 export function getEvents(): { [E in ClientEvent]?: Event } {
-	for (const eventName in preEvents) {
-		const event = preEvents[eventName];
-		if (!event) continue;
-		const old = events[eventName];
-		if (old) {
-			events[eventName] = async function (...args) {
-				if (await event(...args)) old(...args);
-			};
-		} else {
-			events[eventName] = event;
-		}
+	const parsedEvents: Record<string, Event> = {};
+
+	for (const eventName in allEvents) {
+		const preEvent = preEvents[eventName];
+		const events = allEvents[eventName] ?? [];
+
+		const event = async (...args: any) => {
+			const results = await Promise.allSettled(events.map((event) => event(...args)));
+			const failures = results.filter(
+				(result): result is PromiseRejectedResult => result.status === "rejected",
+			);
+
+			if (failures.length === 1) throw failures[0]?.reason;
+			if (failures.length) throw AggregateError(failures);
+		};
+
+		parsedEvents[eventName] = preEvent
+			? async function (...args: any) {
+					if (await preEvent(...args)) await event(...args);
+			  }
+			: event;
 	}
-	return events;
+
+	return parsedEvents;
 }
