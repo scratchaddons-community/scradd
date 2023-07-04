56378e329a6da41fe2351e4558bf300997a912ff
RedGuy12 <paul@reid-family.org>
2023-07-04 18:52:10 -0500

    remove some dead server code
    Signed-off-by: RedGuy12 <paul@reid-family.org>

--- web/server.ts
+++ web/server.ts
@@ -1,102 +1,22 @@
 import { cleanDatabaseListeners } from "../common/database.js";
 import http from "node:http";
-import path from "node:path";
-import fileSystem from "node:fs/promises";
-import mime from "mime-types";
-import { createRequire } from "node:module";
 import { client } from "strife.js";
-// import config from "../common/config.js";
-// import { REST, RESTGetAPIUserResult, RESTPostOAuth2AccessTokenResult, Routes } from "discord.js";
 
-const require = createRequire(import.meta.url);
-
-mime.types.ts = "text/plain";
-const PACKAGE_RESOLVES: Record<string, string> = {};
-// const rest = new REST({ authPrefix: "Bearer" });
 http.createServer((request, response) => {
 	try {
 		const requestUrl = new URL(request.url ?? "", `https://${request.headers.host}`);
 
-		const packageName = PACKAGE_RESOLVES[requestUrl.pathname];
-		if (packageName) {
-			const resolved = require.resolve(packageName);
-
-			return fileSystem.readFile(resolved, "utf8").then((file) => {
-				if (path.extname(resolved) === ".js") {
-					file = file.replaceAll(
-						/import\s+(?<importedNames>.+?)\s+from\s+['"`](?<moduleName>[^.].*?)['"`]/gms,
-						'import $<importedNames> from "./$<moduleName>.js"',
-					);
-				}
-
-				response
-					.writeHead(200, { "Content-Type": mime.lookup(resolved) || "text/plain" })
-					.end(file);
-			});
-		}
-
-		switch (requestUrl.pathname) {
-			case "/clean-database-listeners": {
-				if (requestUrl.searchParams.get("auth") !== process.env.CDBL_AUTH)
-					response.writeHead(403, { "Content-Type": "text/plain" }).end("Forbidden");
-				else {
-					process.emitWarning("cleanDatabaseListeners called");
-					client.user.setPresence({ status: "dnd" });
-					cleanDatabaseListeners().then(() => {
-						process.emitWarning("cleanDatabaseListeners ran");
-						response.writeHead(200, { "Content-Type": "text/plain" }).end("Success");
-					});
-				}
-				break;
+		if (requestUrl.pathname === "/clean-database-listeners") {
+			if (requestUrl.searchParams.get("auth") !== process.env.CDBL_AUTH)
+				response.writeHead(403, { "Content-Type": "text/plain" }).end("Forbidden");
+			else {
+				process.emitWarning("cleanDatabaseListeners called");
+				client.user.setPresence({ status: "dnd" });
+				cleanDatabaseListeners().then(() => {
+					process.emitWarning("cleanDatabaseListeners ran");
+					response.writeHead(200, { "Content-Type": "text/plain" }).end("Success");
+				});
 			}
-			// case "ban-appeal": {
-			// 	const code = requestUrl.searchParams.get("code");
-			// 	if (!code) {
-			// 		response.writeHead(302, {
-			// 			Location: `https://discord.com/api/oauth2/authorize?client_id=${
-			// 				client.user.id
-			// 			}&redirect_uri=${encodeURIComponent(
-			// 				`${requestUrl.origin}/${requestUrl.pathname}`,
-			// 			)}&response_type=code&scope=identify&prompt=none`,
-			// 		});
-			// 		break;
-			// 	}
-			// 	const data = (await rest.post(Routes.oauth2TokenExchange(), {
-			// 		auth: false,
-			// 		body: new URLSearchParams({
-			// 			client_id: client.user.id,
-			// 			client_secret: process.env.BOT_SECRET,
-			// 			grant_type: "authorization_code",
-			// 			code: code,
-			// 			redirect_uri: `${requestUrl.origin}/${requestUrl.pathname}`,
-			// 		}),
-			// 	})) as RESTPostOAuth2AccessTokenResult;
-
-			// 	rest.setToken(data.access_token);
-
-			// 	const user = (await rest.get(Routes.user())) as RESTGetAPIUserResult;
-
-			// 	const ban = await config.guild.bans.fetch(user.id).catch(() => {});
-			// 	if (!ban) {
-			// 		return; // user is not banned
-			// 	}
-
-			// 	const userPublic = {
-			// 		id: user.id,
-			// 		avatar: user.avatar,
-			// 		displayName: user.displayName,
-			// 		ban: ban.reason,
-			// 	};
-
-			// 	return {
-			// 		statusCode: 303,
-			// 		headers: {
-			// 			Location: `/form?token=${encodeURIComponent(
-			// 				createJwt(userPublic, data.expires_in),
-			// 			)}`,
-			// 		},
-			// 	};
-			// }
 		}
 	} catch (error) {
 		response.writeHead(500).end(error.name + ": " + error.message);
