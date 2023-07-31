4bd8eda771ad68e0ee786d26a492ded9919b821f
RedGuy12 <paul@reid-family.org>
2023-07-30 19:05:01 -0500

    expose more DAD code 👀
    Signed-off-by: RedGuy12 <paul@reid-family.org>

--- secrets.ts
+++ secrets.ts
@@ -1,13 +1,42 @@
 import type { User } from "discord.js";
 
 export function dad(name: string, _: User) {
-	return `Hi ${name.split(/\b/)[0] ?? name}, I’m Scradd!`;
+	const split = name.split(/\b/);
+	const firstName =
+		split.find(
+			(word) =>
+				customResponses[word] ||
+				customNames[word] ||
+				customComments[word] ||
+				customTriggers.includes(word),
+		) ??
+		split[0] ??
+		name;
+	const customName = customNames[firstName] ?? name;
+	const comment = customComments[firstName] ?? "I’m Scradd!";
+
+	const greetingIndex = Math.floor(Math.random() * greetings.length);
+	const greeting = greetings[greetingIndex];
+
+	const response =
+		customResponses[firstName] ??
+		`${greeting} ${customName}, ${comment}`;
+
+	return response;
 }
 
-export const dadEasterEggCount = 0;
+const greetings = ["Hi"] as const;
+const customResponses: Record<string, string> = {};
+const customNames: Record<string, string> = {};
+const customComments: Record<string, string> = {};
+const customTriggers: string[] = [];
+export const dadEasterEggCount =
+	Object.keys(customResponses).length +
+	Object.keys(customNames).length +
+	Object.keys(customComments).length +
+	customTriggers.length;
 
-//todo
-// const ignoreTriggers = ["kill", "suicid", "depress", "pain", "sick", "abuse"];
+// const ignoreTriggers = ["kill", "suicid", "depress", "pain", "sick", "abus", "sad", "bleed"] as const; // todo
 
 /**
  * - `word`
