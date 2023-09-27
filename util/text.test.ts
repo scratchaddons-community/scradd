63cbecf0082ac5dbd8c2bedeefb1e47c295b834f
RedGuy12 <paul@reid-family.org>
2023-09-27 08:43:23 -0500

    apparently tests are async now
    Signed-off-by: RedGuy12 <paul@reid-family.org>

--- util/text.test.ts
+++ util/text.test.ts
@@ -2,127 +2,127 @@ import { deepStrictEqual, strictEqual } from "node:assert";
 import { describe, it } from "node:test";
 import { caesar, joinWithAnd, normalize, trimPatchVersion, truncateText } from "./text.js";
 
-describe("joinWithAnd", () => {
-	it("should join an array of 3 items correctly", () => {
+await describe("joinWithAnd", async () => {
+	await it("should join an array of 3 items correctly", () => {
 		strictEqual(joinWithAnd(["foo", "bar", "baz"]), "foo, bar, and baz");
 	});
-	it("should join an array of 2 items correctly", () => {
+	await it("should join an array of 2 items correctly", () => {
 		strictEqual(joinWithAnd(["foo", "bar"]), "foo and bar");
 	});
-	it("should handle an array of 1 item correctly", () => {
+	await it("should handle an array of 1 item correctly", () => {
 		strictEqual(joinWithAnd(["one"]), "one");
 	});
-	it("should handle an empty array correctly", () => {
+	await it("should handle an empty array correctly", () => {
 		strictEqual(joinWithAnd([]), "");
 	});
-	it("should join an array of 3 items with a callback correctly", () => {
+	await it("should join an array of 3 items with a callback correctly", () => {
 		strictEqual(
 			joinWithAnd([1, 2, 3], (item) => `item${item}`),
 			"item1, item2, and item3",
 		);
 	});
-	it("should join an array of 2 items with a callback correctly", () => {
+	await it("should join an array of 2 items with a callback correctly", () => {
 		strictEqual(
 			joinWithAnd([1, 2], (item) => `item${item}`),
 			"item1 and item2",
 		);
 	});
-	it("should join an array of 1 item with a callback correctly", () => {
+	await it("should join an array of 1 item with a callback correctly", () => {
 		strictEqual(
 			joinWithAnd([1], (item) => `item${item}`),
 			"item1",
 		);
 	});
-	it("shouldn’t change the original array", () => {
+	await it("shouldn’t change the original array", () => {
 		const array = ["foo", "bar", "baz"];
 		joinWithAnd(array);
 		deepStrictEqual(array, ["foo", "bar", "baz"]);
 	});
 });
 
-describe("truncateText", () => {
-	it("shouldn’t have trailing spaces", () => {
+await describe("truncateText", async () => {
+	await it("shouldn’t have trailing spaces", () => {
 		strictEqual(
 			truncateText("This is a very long text that needs to be truncated.", 20),
 			"This is a very long…",
 		);
 	});
-	it("should not truncate if maxLength is greater than text length", () => {
+	await it("should not truncate if maxLength is greater than text length", () => {
 		strictEqual(truncateText("Short text", 50), "Short text");
 	});
-	it("should not truncate if maxLength is equal to text length", () => {
+	await it("should not truncate if maxLength is equal to text length", () => {
 		strictEqual(truncateText("Short text", 10), "Short text");
 	});
-	it("should split at newlines", () => {
+	await it("should split at newlines", () => {
 		strictEqual(truncateText("First line\nSecond line", 25), "First line…");
 	});
-	it("shouldn’t split at newlines with multiline on", () => {
+	await it("shouldn’t split at newlines with multiline on", () => {
 		strictEqual(truncateText("First line\nSecond line", 25, true), "First line\nSecond line");
 	});
-	it("should count the …", () => {
+	await it("should count the …", () => {
 		strictEqual(truncateText("foobar", 4), "foo…");
 	});
 });
 
-describe("caesar", () => {
-	it("should handle empty string input", () => {
+await describe("caesar", async () => {
+	await it("should handle empty string input", () => {
 		strictEqual(caesar(""), "");
 	});
-	it("should basically rotate correctly", () => {
+	await it("should basically rotate correctly", () => {
 		strictEqual(caesar("hello"), "uryyb");
 	});
-	it("should handle rotation with a custom rotation", () => {
+	await it("should handle rotation with a custom rotation", () => {
 		strictEqual(caesar("hello", 3), "khoor");
 	});
-	it("should handle rotation with negative rotation", () => {
+	await it("should handle rotation with negative rotation", () => {
 		strictEqual(caesar("khoor", -3), "hello");
 	});
-	it("should handle rotation around the alphabet", () => {
+	await it("should handle rotation around the alphabet", () => {
 		strictEqual(caesar("zebra", 1), "afcsb");
 	});
-	it("should handle mixed case input", () => {
+	await it("should handle mixed case input", () => {
 		strictEqual(caesar("AbCdEf", 2), "CdEfGh");
 	});
-	it("should ignore non-alphabetic characters", () => {
+	await it("should ignore non-alphabetic characters", () => {
 		strictEqual(caesar("Hello, World 10!", 5), "Mjqqt, Btwqi 10!");
 	});
-	it("should handle large rotations", () => {
+	await it("should handle large rotations", () => {
 		strictEqual(caesar("Hello", 26), "Hello");
 	});
 });
 
-describe("normalize", () => {
-	it("should remove diacritics", () => {
+await describe("normalize", async () => {
+	await it("should remove diacritics", () => {
 		strictEqual(normalize("Café"), "Cafe");
 	});
-	it("should condense whitespace", () => {
+	await it("should condense whitespace", () => {
 		strictEqual(normalize("  This  is\t a\n test  \rstring. "), " This is a\n test \nstring. ");
 	});
-	it("should normalize line endings", () => {
+	await it("should normalize line endings", () => {
 		strictEqual(
 			normalize("Line 1\r\nLine 2\nLine 3\rLine 4"),
 			"Line 1\nLine 2\nLine 3\nLine 4",
 		);
 	});
-	it("should ignore empty input", () => {
+	await it("should ignore empty input", () => {
 		strictEqual(normalize(""), "");
 	});
-	it("should ignore normal text", () => {
+	await it("should ignore normal text", () => {
 		strictEqual(normalize("This is a test string."), "This is a test string.");
 	});
 });
 
-describe("trimPatchVersion", () => {
-	it("should trim the patch version", () => {
+await describe("trimPatchVersion", async () => {
+	await it("should trim the patch version", () => {
 		strictEqual(trimPatchVersion("1.0.0"), "1.0");
 	});
-	it("should trim multiple patch versions", () => {
+	await it("should trim multiple patch versions", () => {
 		strictEqual(trimPatchVersion("1.0.0"), "1.0");
 	});
-	it("should ignore versions without a patch", () => {
+	await it("should ignore versions without a patch", () => {
 		strictEqual(trimPatchVersion("1.0"), "1.0");
 	});
-	it("should ignore invalid versions", () => {
+	await it("should ignore invalid versions", () => {
 		strictEqual(trimPatchVersion("1.0"), "1.0");
 	});
 });
