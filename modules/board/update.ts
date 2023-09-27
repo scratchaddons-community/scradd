2a8fd2ea3644cf3446ffa96832947bf8b31e4597
RedGuy12 <paul@reid-family.org>
2023-09-26 19:13:49 -0500

    toSpliced
    Signed-off-by: RedGuy12 <paul@reid-family.org>

--- modules/board/update.ts
+++ modules/board/update.ts
@@ -78,7 +78,7 @@ export default async function updateBoard(
 		}),
 	);
 	const pins = await config.channels.board.messages.fetchPinned();
-	if (pins.size > top.length) {
+	if (pins.size > topIds.length) {
 		await Promise.all(
 			pins
 				.filter((pin) => !topIds.includes(pin.id))
