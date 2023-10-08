b2db0c958e8a99a1a7e655ee6332ad8cc8a6c569
RedGuy12 <paul@reid-family.org>
2023-10-07 22:19:01 -0500

    refactor a bit, fix some bugs detected by ts and eslint, use a cleaner code style
    Signed-off-by: RedGuy12 <paul@reid-family.org>

--- web/server.ts
+++ web/server.ts
@@ -1,105 +1,85 @@
 import { cleanDatabaseListeners } from "../common/database.js";
 import http from "node:http";
 import logError from "../modules/logging/errors.js";
-import fs from "fs";
+import fileSystem from "node:fs/promises";
 import { client } from "strife.js";
 
 http.createServer(async (request, response) => {
 	try {
 		const requestUrl = new URL(request.url ?? "", `https://${request.headers.host}`);
-		const params = new URLSearchParams(new URL(request.url ?? "").search);
 
-		if (requestUrl.pathname === "/clean-database-listeners") {
-			if (requestUrl.searchParams.get("auth") === process.env.CDBL_AUTH) {
+		switch (requestUrl.pathname) {
+			case "/clean-database-listeners": {
+				if (requestUrl.searchParams.get("auth") !== process.env.CDBL_AUTH)
+					response.writeHead(403, { "Content-Type": "text/plain" }).end("Forbidden");
+
 				process.emitWarning("cleanDatabaseListeners called");
-				cleanDatabaseListeners().then(
-					() => {
-						process.emitWarning("cleanDatabaseListeners ran");
-						response.writeHead(200, { "Content-Type": "text/plain" }).end("Success");
-					},
-					(error) => logError(error, request.url ?? ""),
-				);
-			} else {
-				response.writeHead(403, { "Content-Type": "text/plain" }).end("Forbidden");
+				await cleanDatabaseListeners();
+				process.emitWarning("cleanDatabaseListeners ran");
+				response.writeHead(200, { "Content-Type": "text/plain" }).end("Success");
+
+				break;
 			}
-		} else if (requestUrl.pathname === "/appeal") {
-			if (!params?.get("code")) return;
-			let codeParam: string = params.get("code") || "";
-			let tokenResponseData = await (
-				await fetch("https://discord.com/api/oauth2/token", {
-					method: "POST",
-					body: new URLSearchParams({
-						client_id: "929928324959055932",
-						client_secret: "",
-						code: codeParam,
-						grant_type: "authorization_code",
-						redirect_uri: "https://sa-discord.up.railway.app/appeal",
-						scope: "identify",
-					}).toString(),
-					headers: {
-						"Content-Type": "application/x-www-form-urlencoded",
-					},
-				})
-			).json();
-			let token_type: string = (tokenResponseData as any)?.token_type;
-			let access_token: string = (tokenResponseData as any)?.access_token;
-			if (access_token) {
-				let user = await (
+			case "/appeal": {
+				const code = new URLSearchParams(new URL(request.url ?? "").search).get("code");
+				if (!code) return;
+
+				const tokenResponseData = await (
+					await fetch("https://discord.com/api/oauth2/token", {
+						method: "POST",
+						body: new URLSearchParams({
+							client_id: "929928324959055932",
+							client_secret: "",
+							code,
+							grant_type: "authorization_code",
+							redirect_uri: "https://sa-discord.up.railway.app/appeal",
+							scope: "identify",
+						}).toString(),
+						headers: { "Content-Type": "application/x-www-form-urlencoded" },
+					})
+				).json();
+				const token_type: string = (tokenResponseData as any)?.token_type;
+				const access_token: string = (tokenResponseData as any)?.access_token;
+				if (!access_token) {
+					response
+						.writeHead(503, { "Content-Type": "text/plain" })
+						.end("Verification Failed");
+				}
+
+				const user = await (
 					await fetch("https://discord.com/api/users/@me", {
 						headers: {
 							authorization: `${token_type} ${access_token}`,
 						},
 					})
 				).json();
-				let userId: string = (user as any)?.id;
-				if (userId) {
-					let bannedUser: object = (await client.users.fetch(userId).catch()) || {
-						username: "",
-					};
-					fs.readFile("./appeal/appeal.html", function (err, html) {
-						if (err) {
-							throw err;
-						}
-						response
-							.writeHead(200, { "Content-Type": "text/html" })
-							.end(
-								html
-									.toString()
-									.replaceAll("{username}", (bannedUser as any)?.username || ""),
-							);
-					});
-				} else {
+				const userId: string = (user as any)?.id;
+				if (!userId)
 					response
 						.writeHead(503, { "Content-Type": "text/plain" })
 						.end("Verification Failed");
-				}
-			} else {
+
+				const bannedUser = (await client.users.fetch(userId).catch(() => void 0)) || {
+					username: "",
+				};
+				const html = await fileSystem.readFile("./appeal/appeal.html", "utf8");
 				response
-					.writeHead(503, { "Content-Type": "text/plain" })
-					.end("Verification Failed");
+					.writeHead(200, { "Content-Type": "text/html" })
+					.end(html.replaceAll("{username}", (bannedUser as any)?.username || ""));
+				break;
 			}
-		} else if ((requestUrl.pathname = "/appeal.css")) {
-			fs.readFile("./appeal/appeal.css", function (err, css) {
-				if (err) {
-					throw err;
-				}
+			case "/appeal.css": {
+				const css = await fileSystem.readFile("./appeal/appeal.css");
 				response.writeHead(200, { "Content-Type": "text/css" }).end(css);
-			});
-		} else if ((requestUrl.pathname = "/scradd.svg")) {
-			fs.readFile("./appeal/scradd.svg", function (err, css) {
-				if (err) {
-					throw err;
-				}
-				response.writeHead(200, { "Content-Type": "text/css" }).end(css);
-			});
-		} else {
-			response.writeHead(404, { "Content-Type": "text/plain" }).end("Not Found");
+				break;
+			}
+			default: {
+				response.writeHead(404, { "Content-Type": "text/plain" }).end("Not Found");
+			}
 		}
 	} catch (error) {
 		response.writeHead(500).end("Internal Server Error");
-		logError(error, request.url ?? "").catch((error) => {
-			throw error;
-		});
+		await logError(error, request.url ?? "");
 	}
 }).listen(process.env.PORT, () => {
 	console.log("Server up!");
