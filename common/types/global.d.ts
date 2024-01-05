7b0d02e893a04f619f600f69d58d5152dcb67d5f
RedGuy12 <RedGuy12@users.noreply.github.com>
2024-01-05 01:02:52 +0000

    Format

--- common/types/global.d.ts
+++ common/types/global.d.ts
@@ -94,16 +94,17 @@ declare global {
 
 	namespace NodeJS {
 		/**
-  @example ```env
-GUILD_ID = ...
-BOT_TOKEN = ...
-MONGO_URI = mongodb://127.0.0.1:27017/scradd
-NODE_ENV = development
-CANVAS = true
-PORT = 80 ; Only if you want web server features (including ban appeals and linking Scratch)
-CLIENT_SECRET = ...
-```
-*/
+		 * @example
+		 * 	```env
+		 * 	GUILD_ID = ...
+		 * 	BOT_TOKEN = ...
+		 * 	MONGO_URI = mongodb://127.0.0.1:27017/scradd
+		 * 	NODE_ENV = development
+		 * 	CANVAS = true
+		 * 	PORT = 80 ; Only if you want web server features (including ban appeals and linking Scratch)
+		 * 	CLIENT_SECRET = ...
+		 * 	```;
+		 */
 		interface ProcessEnv {
 			/** The main guild ID for the bot to operate in. Requires Administrator permission in this server. */
 			GUILD_ID: Snowflake;
