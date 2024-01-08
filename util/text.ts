dbe2430e1384df6ae083d47d3e9220b2ee336423
RedGuy12 <paul@reid-family.org>
2024-01-07 18:12:55 -0600

    fix edgecases in getRequestUrl
    Signed-off-by: RedGuy12 <paul@reid-family.org>

--- util/text.ts
+++ util/text.ts
@@ -116,8 +116,9 @@ export function trimPatchVersion(full: string): string {
 export function getRequestUrl(request: IncomingMessage) {
 	return new URL(
 		request.url ?? "",
-		request.headers["x-forwarded-host"]
-			? `${request.headers["x-forwarded-proto"]}://${request.headers["x-forwarded-host"]}`
-			: `http${"encrypted" in request.socket ? "s" : ""}://${request.headers.host}`,
+		`${
+			request.headers["x-forwarded-proto"] ||
+			`http${"encrypted" in request.socket ? "s" : ""}`
+		}://${request.headers["x-forwarded-host"] || request.headers.host}`,
 	);
 }
