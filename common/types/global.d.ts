002e4a8bcc15ed0b5b115551f4d76b465e45b24e
RedGuy12 <paul@reid-family.org>
2024-01-07 18:08:03 -0600

    change .split type override
    Signed-off-by: RedGuy12 <paul@reid-family.org>

--- common/types/global.d.ts
+++ common/types/global.d.ts
@@ -77,7 +77,7 @@ declare global {
 		readonly prototype: Boolean;
 	}
 	interface String {
-		split<Separator extends string, Limit extends number>(
+		split<Separator extends RegExp | string, Limit extends number>(
 			separator: Separator,
 			limit?: Limit,
 		): Limit extends 0 ? [] : Separator extends "" ? string[] : [string, ...string[]];
