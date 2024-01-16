86f0b3c95aac31949108d1201507f5214327ba11
RedGuy12 <paul@reid-family.org>
2024-01-15 19:47:36 -0600

    change former mod to former staff
    Signed-off-by: RedGuy12 <paul@reid-family.org>

--- modules/roles/persisted.ts
+++ modules/roles/persisted.ts
@@ -6,7 +6,7 @@ export const persistedRoles = {
 	designer: "916020774509375528",
 	scradd: "1008190416396484700",
 	admin: ["1069776422467555328", "806603332944134164"],
-	mod: ["881623848137682954", config.roles.mod?.id],
+	mod: ["881623848137682954", config.roles.staff?.id],
 	dev: "806608777835053098",
 	translator: "841696608592330794",
 	contributor: config.roles.dev?.id,
