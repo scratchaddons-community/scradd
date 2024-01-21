3d2b0ed8fe132794ba31bf0b2e507c3b92247ac3
Yande <113806118+YandeMC@users.noreply.github.com>
2024-01-20 20:58:28 -0500

    replace footer text with button
    untested

--- modules/auto/index.ts
+++ modules/auto/index.ts
@@ -6,6 +6,8 @@ import {
 	ApplicationCommandType,
 	type Snowflake,
 	type APIEmbed,
+	ComponentType,
+	ButtonStyle,
 } from "discord.js";
 import { getSettings } from "../settings.js";
 import { BOARD_EMOJI } from "../board/misc.js";
@@ -140,10 +142,21 @@ async function handleMutatable(message: Message) {
 			const embed = await handleMatch(match);
 			if (embed) {
 				embeds.push(embed);
-				if (notSet) embed.footer = { text: "Disable this using /settings" };
 			}
 		}
-		if (embeds.length) return { content: "", files: [], embeds, components: [] };
+		if (embeds.length) return { content: "", files: [], embeds, components: notSet ? [
+			{
+			components: [
+				{
+					customId: "scratchEmbeds_toggleSetting",
+					type: ComponentType.Button as const,
+					label: `Disable Scratch Embeds`,
+					style: ButtonStyle.Success as const,
+				},
+			],
+			type: ComponentType.ActionRow,
+		}
+	] : [] };
 	}
 
 	const ignored = ignoreTriggers.some((trigger) => message.content.match(trigger));
