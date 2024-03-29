ab9c70c6deabbe637f88e3c7dd414f804089eb4f
cobalt <61329810+RedGuy12@users.noreply.github.com>
2024-03-29 14:57:46 -0500

    Ignore failed join dms
    Signed-off-by: cobalt <61329810+RedGuy12@users.noreply.github.com>

--- modules/joins.ts
+++ modules/joins.ts
@@ -12,82 +12,84 @@ const directoryUrl =
 
 defineEvent("guildMemberAdd", async (member) => {
 	if (member.guild.id !== config.guild.id) return;
-	await member.send({
-		embeds: [
-			{
-				color: constants.themeColor,
-				url: constants.domains.scratchAddons,
-				title: "Welcome to the __Scratch Addons__ Discord server!",
-				description: "Thank you for joining the Scratch Addons community!",
-				fields: [
-					{
-						name: "**What is this server?**",
-						value:
-							`This is *the largest [Scratch](${constants.domains.scratch}) server*!` +
-							` Check out some of our funniest and most memorable moments${config.channels.board ? ` on the ${config.channels.board.toString()}` : ""} and introduce yourself${config.channels.intros ? ` in ${config.channels.intros.toString()}` : ""}.` +
-							(directoryUrl &&
-								` You can also check out our [server directory](<${directoryUrl}>) for other large Scratch servers to chat in.`),
+	await member
+		.send({
+			embeds: [
+				{
+					color: constants.themeColor,
+					url: constants.domains.scratchAddons,
+					title: "Welcome to the __Scratch Addons__ Discord server!",
+					description: "Thank you for joining the Scratch Addons community!",
+					fields: [
+						{
+							name: "**What is this server?**",
+							value:
+								`This is *the largest [Scratch](${constants.domains.scratch}) server*!` +
+								` Check out some of our funniest and most memorable moments${config.channels.board ? ` on the ${config.channels.board.toString()}` : ""} and introduce yourself${config.channels.intros ? ` in ${config.channels.intros.toString()}` : ""}.` +
+								(directoryUrl &&
+									` You can also check out our [server directory](<${directoryUrl}>) for other large Scratch servers to chat in.`),
+						},
+						{
+							name: "**What is Scratch Addons?**",
+							value:
+								"This server focuses specifically on *the Scratch Addons browser extension*, the all-in-one browser extension for Scratch." +
+								" Scratch Addons combines new and existing features and themes for the Scratch website and project editor into one __easy-to-access and configurable__ browser extension." +
+								` For more information about us, **visit [ScratchAddons.com](${constants.domains.scratchAddons})**.`,
+						},
+						{
+							name: "**We are not the Scratch Team.**",
+							value:
+								"Please know that *nobody here is a Scratch developer or moderator*, we’re just some people who like to code, like you!" +
+								` If you wish to contact the ST, please use [Contact Us](<${constants.domains.scratch}/contact-us>).` +
+								" **No official Scratch server exists**, but please feel free to socialize with other Scratchers here.",
+						},
+					],
+					footer: {
+						icon_url: `https://raw.githubusercontent.com/${constants.repos.scratchAddons}/master/images/icon.png`,
+						text: "~ the Scratch Addons team",
 					},
-					{
-						name: "**What is Scratch Addons?**",
-						value:
-							"This server focuses specifically on *the Scratch Addons browser extension*, the all-in-one browser extension for Scratch." +
-							" Scratch Addons combines new and existing features and themes for the Scratch website and project editor into one __easy-to-access and configurable__ browser extension." +
-							` For more information about us, **visit [ScratchAddons.com](${constants.domains.scratchAddons})**.`,
-					},
-					{
-						name: "**We are not the Scratch Team.**",
-						value:
-							"Please know that *nobody here is a Scratch developer or moderator*, we’re just some people who like to code, like you!" +
-							` If you wish to contact the ST, please use [Contact Us](<${constants.domains.scratch}/contact-us>).` +
-							" **No official Scratch server exists**, but please feel free to socialize with other Scratchers here.",
-					},
-				],
-				footer: {
-					icon_url: `https://raw.githubusercontent.com/${constants.repos.scratchAddons}/master/images/icon.png`,
-					text: "~ the Scratch Addons team",
+					image: { url: `${constants.domains.scradd}/images/join-dm-1.png` },
 				},
-				image: { url: `${constants.domains.scradd}/images/join-dm-1.png` },
-			},
-			{
-				url: constants.domains.scratchAddons,
-				image: { url: `${constants.domains.scradd}/images/join-dm-2.png` },
-			},
-			{
-				url: constants.domains.scratchAddons,
-				image: { url: `${constants.domains.scradd}/images/join-dm-3.png` },
-			},
-			{
-				url: constants.domains.scratchAddons,
-				image: { url: `${constants.domains.scradd}/images/join-dm-4.png` },
-			},
-		],
-		components: [
-			{
-				type: ComponentType.ActionRow,
-				components: [
-					{
-						url: constants.domains.scratchAddons,
-						style: ButtonStyle.Link,
-						type: ComponentType.Button,
-						label: "Get Scratch Addons",
-					},
-					{
-						url: config.guild.rulesChannel?.url ?? channelLink("", config.guild.id),
-						style: ButtonStyle.Link,
-						type: ComponentType.Button,
-						label: "Server Rules",
-					},
-					{
-						url: directoryUrl || channelLink("", config.guild.id),
-						style: ButtonStyle.Link,
-						type: ComponentType.Button,
-						label: "Other Scratch Servers",
-					},
-				],
-			},
-		],
-	});
+				{
+					url: constants.domains.scratchAddons,
+					image: { url: `${constants.domains.scradd}/images/join-dm-2.png` },
+				},
+				{
+					url: constants.domains.scratchAddons,
+					image: { url: `${constants.domains.scradd}/images/join-dm-3.png` },
+				},
+				{
+					url: constants.domains.scratchAddons,
+					image: { url: `${constants.domains.scradd}/images/join-dm-4.png` },
+				},
+			],
+			components: [
+				{
+					type: ComponentType.ActionRow,
+					components: [
+						{
+							url: constants.domains.scratchAddons,
+							style: ButtonStyle.Link,
+							type: ComponentType.Button,
+							label: "Get Scratch Addons",
+						},
+						{
+							url: config.guild.rulesChannel?.url ?? channelLink("", config.guild.id),
+							style: ButtonStyle.Link,
+							type: ComponentType.Button,
+							label: "Server Rules",
+						},
+						{
+							url: directoryUrl || channelLink("", config.guild.id),
+							style: ButtonStyle.Link,
+							type: ComponentType.Button,
+							label: "Other Scratch Servers",
+						},
+					],
+				},
+			],
+		})
+		.catch(() => void 0);
 });
 
 defineEvent("guildMemberAdd", async (member) => {
