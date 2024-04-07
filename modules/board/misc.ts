663c45da423aba1f76f10fc549ecaa04a3eb77d4
cobalt <61329810+RedGuy12@users.noreply.github.com>
2024-04-07 12:45:18 -0500

    fix(board): lower decay speed
    Signed-off-by: cobalt <61329810+RedGuy12@users.noreply.github.com>

--- modules/board/misc.ts
+++ modules/board/misc.ts
@@ -18,6 +18,9 @@ import tryCensor, { censor } from "../automod/misc.js";
 export const BOARD_EMOJI = process.env.NODE_ENV === "production" ? "🥔" : "⭐",
 	REACTIONS_NAME = process.env.NODE_ENV === "production" ? "Potatoes" : "Stars";
 
+/** Number of days for required potato count to double. */
+const DECAY_SPEED = 750;
+
 export const boardDatabase = new Database<{
 	/** The number of reactions this message has. */
 	reactions: number;
@@ -86,8 +89,7 @@ export function boardReactionCount(
 			channel instanceof BaseChannel && channel.type === ChannelType.PrivateThread ?
 				2 / 3
 			:	1;
-		/** 500 = number of days for required potato count to double. */
-		const timeShift = (Date.now() - +time) / 86_400_000 / 500 + 1;
+		const timeShift = (Date.now() - +time) / 86_400_000 / DECAY_SPEED + 1;
 		return Math.max(2, Math.round(count * privateThread * timeShift));
 	}
 }
