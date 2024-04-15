6ebdbd689d8a2769146e01578a1d5d0bdc2d0ebd
cobalt <61329810+RedGuy12@users.noreply.github.com>
2024-04-15 17:37:07 -0500

    impr: Remove `/poll` (#642)
    Also refactors the code to be focused on QOTDs
    
    The main downside of switching to Discord polls (for us) is that they have an expiry, Scradd's don't and were able to run indefinitely. If this is too much of a trade-off, I can keep `/poll`. Let me know what you think.
    
    Old polls with `vote-mode: True` will still be enforced.
    
    closes #634 
    closes #333

---
+++ modules/qotd/add.ts
@@ -0,0 +1,128 @@
+import {
+	ButtonStyle,
+	ComponentType,
+	TextInputStyle,
+	type ChatInputCommandInteraction,
+	type ModalSubmitInteraction,
+} from "discord.js";
+import constants from "../../common/constants.js";
+import tryCensor from "../automod/misc.js";
+import warn from "../punishments/warn.js";
+import { DEFAULT_SHAPES, parseOptions } from "./misc.js";
+import { Question, questions } from "./send.js";
+
+export default async function getQuestionData(
+	interaction: ChatInputCommandInteraction,
+): Promise<void> {
+	await interaction.showModal({
+		title: "Add A Question of The Day",
+		customId: "_addQuestion",
+		components: [
+			{
+				type: ComponentType.ActionRow,
+				components: [
+					{
+						type: ComponentType.TextInput,
+						style: TextInputStyle.Short,
+						label: "The question to ask",
+						required: true,
+						customId: "question",
+						maxLength: 256,
+					},
+				],
+			},
+			{
+				type: ComponentType.ActionRow,
+				components: [
+					{
+						type: ComponentType.TextInput,
+						style: TextInputStyle.Paragraph,
+						label: `Extended description`,
+						required: false,
+						customId: "description",
+					},
+				],
+			},
+			{
+				type: ComponentType.ActionRow,
+				components: [
+					{
+						type: ComponentType.TextInput,
+						style: TextInputStyle.Paragraph,
+						label: `Answers (one per line; max of ${DEFAULT_SHAPES.length})`,
+						placeholder: "👍 Yes\n👎 No",
+						required: false,
+						customId: "answers",
+					},
+				],
+			},
+			// TODO: Specify dates or ranges of dates
+		],
+	});
+}
+export async function addQuestion(interaction: ModalSubmitInteraction): Promise<void> {
+	const question = interaction.fields.getTextInputValue("question").trim();
+	const rawDescription = interaction.fields.fields.get("description")?.value.trim();
+	const rawOptions = interaction.fields.fields.get("answers")?.value.trim() ?? "";
+	const description = (rawDescription ?? "") + (rawDescription && rawOptions ? "\n\n" : "");
+	const toCensor = `${question}${
+		description || rawOptions ? "\n\n\n" : ""
+	}${description}${rawOptions}`;
+	const censored = tryCensor(toCensor);
+	if (censored) {
+		await warn(
+			interaction.user,
+			censored.words.length === 1 ? "Used a banned word" : "Used banned words",
+			censored.strikes,
+			`Attempted to create QOTD:\n>>> ${toCensor}`,
+		);
+		await interaction.reply({
+			content: `${constants.emojis.statuses.no} Please ${
+				censored.strikes < 1 ? "don’t say that here" : "watch your language"
+			}!`,
+			ephemeral: true,
+		});
+		return;
+	}
+
+	const { options, reactions } = parseOptions(rawOptions);
+	if (options.length !== reactions.length) {
+		await interaction.reply({
+			content: `${constants.emojis.statuses.no} You can’t have over ${
+				DEFAULT_SHAPES.length
+			} option${DEFAULT_SHAPES.length === 1 ? "" : "s"}!`,
+			ephemeral: true,
+		});
+		return;
+	}
+
+	const fullDescription = `${description}${reactions
+		.map((reaction, index) => `${reaction} ${options[index] ?? ""}`)
+		.join("\n")}`;
+	questions.push(
+		await new Question({
+			question,
+			description: fullDescription,
+			reactions,
+			_id: interaction.id,
+		}).save(),
+	);
+
+	await interaction.reply({
+		content: constants.emojis.statuses.yes + " Added question!",
+		embeds: [{ color: constants.themeColor, title: question, description: fullDescription }],
+		components: [
+			{
+				type: ComponentType.ActionRow,
+				components: [
+					{
+						customId: interaction.id + "_removeQuestion",
+						type: ComponentType.Button,
+						label: "Remove",
+						style: ButtonStyle.Danger,
+					},
+				],
+			},
+		],
+	});
+}
---
+++ modules/qotd/index.ts
@@ -0,0 +1,66 @@
+import { ChannelType } from "discord.js";
+import {
+	client,
+	defineButton,
+	defineEvent,
+	defineModal,
+	defineSelect,
+	defineSubcommands,
+} from "strife.js";
+import config from "../../common/config.js";
+import getQuestionData, { addQuestion } from "./add.js";
+import { listQuestions, removeQuestion, viewQuestion } from "./list.js";
+
+defineSubcommands(
+	{
+		name: "qotd",
+		description: "Manage Questions of The Day",
+
+		subcommands: {
+			add: { description: "Add a Question of The Day" },
+			list: { description: "List Questions of The Day" },
+		},
+
+		restricted: true,
+	},
+	async (interaction, { subcommand }) => {
+		switch (subcommand) {
+			case "add": {
+				await getQuestionData(interaction);
+				break;
+			}
+			case "list": {
+				await listQuestions(interaction);
+				break;
+			}
+		}
+	},
+);
+defineModal("addQuestion", addQuestion);
+
+defineSelect("viewQuestion", viewQuestion);
+defineButton("removeQuestion", removeQuestion);
+
+defineEvent("messageReactionAdd", async (partialReaction, partialUser) => {
+	const reaction = partialReaction.partial ? await partialReaction.fetch() : partialReaction;
+	const message = reaction.message.partial ? await reaction.message.fetch() : reaction.message;
+	const user = partialUser.partial ? await partialUser.fetch() : partialUser;
+
+	if (message.author.id !== client.user.id) return;
+	if (
+		(message.interaction?.commandName !== "poll" || !message.embeds[0]?.footer?.text) &&
+		(message.channel.type !== ChannelType.PublicThread ||
+			message.channel.parent?.id !== config.channels.qotd)
+	)
+		return;
+
+	const emojis = (message.embeds[0]?.description ?? message.content).match(/^\S+/gm);
+	if (!reaction.emoji.name || !emojis?.includes(reaction.emoji.name)) return;
+
+	for (const [, other] of message.reactions
+		.valueOf()
+		.filter(
+			({ emoji }) => emoji.name !== reaction.emoji.name && emojis.includes(emoji.name || "_"),
+		))
+		await other.users.remove(user);
+});
