0859ee9f26148a8398ede49a5c77135fb1068cc4
Yande <yande@yande.dev>
2024-05-06 19:24:01 -0400

    combine the guards

--- modules/joins.ts
+++ modules/joins.ts
@@ -93,13 +93,11 @@ defineEvent("guildMemberAdd", async (member) => {
 });
 
 defineEvent("guildMemberUpdate", async (_, member) => {
-	if (!features.joinsAutoKick) return;
-	if (!config.roles.autoKick) return;
-	if (!member.roles.resolve(config.roles.autoKick.id)) return;
+	if (!features.joinsAutoKick || !config.roles.autoKick || !member.roles.resolve(config.roles.autoKick.id)) return;
 
 	member.joinedTimestamp && member.joinedTimestamp > Date.now() - 60 * 60 * 1000 ?
 		await member.kick("Autokick role assigned")
-	:	await member.roles.remove(config.roles.autoKick, "Autokick role assigned post-join");
+		: await member.roles.remove(config.roles.autoKick, "Autokick role assigned post-join");
 });
 
 defineEvent("guildMemberAdd", async (member) => {
@@ -108,10 +106,10 @@ defineEvent("guildMemberAdd", async (member) => {
 	const countString = config.guild.memberCount.toString();
 	const jokes =
 		/^[1-9]0+$/.test(countString) ? ` (${"🥳".repeat(countString.length - 1)})`
-		: countString.includes("69") ? " (nice)"
-		: countString.endsWith("87") ?
-			` (WAS THAT THE BITE OF ’87${"⁉".repeat(Math.ceil(countString.length / 2))})`
-		:	"";
+			: countString.includes("69") ? " (nice)"
+				: countString.endsWith("87") ?
+					` (WAS THAT THE BITE OF ’87${"⁉".repeat(Math.ceil(countString.length / 2))})`
+					: "";
 	const memberCount = nth(config.guild.memberCount) + jokes;
 
 	const greeting = joins[Math.floor(Math.random() * joins.length)] ?? joins[0];
