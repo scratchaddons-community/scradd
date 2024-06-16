d0280472920272223b062cee31b8f16b360ee607
RedGuy12 <RedGuy12@users.noreply.github.com>
2024-06-16 00:08:12 +0000

    Format

--- modules/qotds/index.ts
+++ modules/qotds/index.ts
@@ -60,7 +60,8 @@ defineEvent("messageReactionAdd", async (partialReaction, partialUser) => {
 	for (const [, other] of message.reactions
 		.valueOf()
 		.filter(
-			({ emoji }) => emoji.name !== reaction.emoji.name && emojis.includes(emoji.name || "__"),
+			({ emoji }) =>
+				emoji.name !== reaction.emoji.name && emojis.includes(emoji.name || "__"),
 		))
 		await other.users.remove(user);
 });
