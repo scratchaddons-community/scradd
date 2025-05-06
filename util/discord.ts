677bcf311a39e0c9af9b0b8f26f735f15aa4f5d7
cobalt <61329810+cobaltt7@users.noreply.github.com>
2025-05-05 19:36:32 -0500

    Embeds: Check embed permission
    Signed-off-by: cobalt <61329810+cobaltt7@users.noreply.github.com>

--- util/discord.ts
+++ util/discord.ts
@@ -3,18 +3,30 @@ import type { Channel, SendableChannels } from "discord.js";
 import { PermissionFlagsBits } from "discord.js";
 import { client } from "strife.js";
 
-export function assertSendable<T extends Channel>(channel: T): (T & SendableChannels) | undefined {
+export function assertSendable<T extends Channel>(
+	channel: T,
+	additionalPermissions?: bigint,
+): (T & SendableChannels) | undefined {
 	if (!channel.isSendable()) return;
 	if (channel.isDMBased()) return channel;
 
 	if (channel.isThread()) {
 		if (!channel.sendable) return;
+		if (additionalPermissions !== undefined) {
+			const permissions = channel.permissionsFor(client.user);
+			if (!permissions?.has(additionalPermissions)) return;
+		}
 		return channel;
 	}
 
 	const permissions = channel.permissionsFor(client.user);
 	if (!permissions) return;
 
-	if (permissions.has(PermissionFlagsBits.ViewChannel | PermissionFlagsBits.SendMessages))
+	const needed = PermissionFlagsBits.ViewChannel | PermissionFlagsBits.SendMessages;
+	if (
+		permissions.has(
+			additionalPermissions === undefined ? needed : needed | additionalPermissions,
+		)
+	)
 		return channel;
 }
